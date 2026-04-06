7a49a9b1dcf2e0e307f7df2fcaccd178eec0520e
Veries Seals <veries@me.com>
2026-04-06 17:18:21 -0500

    Add import from persons and created ImmigrationStatus class

--- immigration_status/models.py
+++ immigration_status/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from persons.models import Person
 
 # Create your models here.
+# ------------------------------------------
+
+class ImmigrationStatus(models.Model):
+    pass
