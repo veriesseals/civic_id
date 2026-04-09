845dbc173652ba17585acb712db97a6608efe7be
Veries Seals <veries@me.com>
2026-04-08 22:03:26 -0500

    Created class IssuedID in issued_ids models.py

--- issued_ids/models.py
+++ issued_ids/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from persons.models import Person
+from id_applications.models import IDApplication
 
 # Create your models here.
+# ----------------------------------------------
+
+class IssuedID(models.Model):
+    pass
