a6e3640e8f33e6c18d7d78a1c73c7e350eb74c7b
Veries Seals <veries@me.com>
2026-04-10 14:01:42 -0500

    Added logic for id_applications admin.py

--- id_applications/admin.py
+++ id_applications/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import IDApplication
 
 # Register your models here.
+# --------------------------------------------
+
+# Register the IDApplication model with the admin site.
+# ----------------------------------------
+admin.site.register(IDApplication)
