7609e96e0be6294b13cf2a957050461e9195654e
Veries Seals <veries@me.com>
2026-04-11 11:43:06 -0500

    Created in views, class BirthRecordViewSet, defined queryset, defined serializer_class

--- birth_records/views.py
+++ birth_records/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import BirthRecord
+from .serializers import BirthRecordSerializer
 
 # Create your views here.
+# --------------------------------------------------------------
+
+# Using ModelViewSet to automatically provide list, create, retrieve, update, and destroy actions.
+# This allows us to easily manage our BirthRecord objects through the API without having to write additional code for each action.
+# ---------------------------------------------------------------
+class BirthRecordViewSet(viewsets.ModelViewSet):
+    # The queryset defines the set of data that will be used for the viewset. In this case, we are retrieving all BirthRecord objects from the database.
+    # ---------------------------------------------------------------
+    queryset = BirthRecord.objects.all()
+    # The serializer_class specifies the serializer that will be used to convert the BirthRecord objects to and from JSON format. This allows us to easily serialize and deserialize our data when interacting with the API.
+    # ---------------------------------------------------------------
+    serializer_class = BirthRecordSerializer
