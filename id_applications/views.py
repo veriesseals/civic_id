9fa5938bd11d5957f4c9c84a81668e90725bebe7
Veries Seals <veries@me.com>
2026-04-11 13:33:15 -0500

    Updated comments

--- id_applications/views.py
+++ id_applications/views.py
@@ -12,6 +12,6 @@ class IDApplicationViewSet(viewsets.ModelViewSet):
     # The queryset defines the set of data that will be used for the viewset. In this case, we are retrieving all IDApplication objects from the database.
     # ---------------------------------------------------------------
     queryset = IDApplication.objects.all()
-    # The serializer_class specifies the serializer that will be used to convert the IDApplication objects to and from JSON format. This allows us to easily serialize and deserialize our data when interacting with the API.
+    # The serializer_class specifies the serializers that will be used to convert the IDApplication objects to and from JSON format. This allows us to easily serialize and deserialize our data when interacting with the API.
     # ---------------------------------------------------------------
     serializer_class = IDApplicationSerializer
