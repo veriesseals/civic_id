daa336a58c964c5e09e45c23a8c4501a5af42c48
Veries Seals <veries@me.com>
2026-04-11 16:47:21 -0500

    Added serializers.py to immigration_status. Created class ImmigrationStatusSerializer, and class meta. Defined model and fields

---
+++ immigration_status/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import ImmigrationStatus
+
+# Serializer for the ImmigrationStatus model to convert model instances
+# to JSON and vice versa, including all fields
+# ---------------------------------------------------------------------------
+
+class ImmigrationStatusSerializer(serializers.ModelSerializer):
+    # Meta class to specify the model and fields to be included
+    # in the serialization process
+    # --------------------------------------------
+    class Meta:
+        # Specify the model to be serialized
+        # --------------------------------------------
+        model = ImmigrationStatus
+        # Include all fields from the model in the serialization
+        # 
+        fields = "__all__"
