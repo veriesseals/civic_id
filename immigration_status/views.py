35bcc124da7f9d42ddca9103f1d1f4035944e2eb
Veries Seals <veries@me.com>
2026-04-11 18:10:34 -0500

    In immigration_status created class ImmigrationStatusViewSet, defined queryset and serializer_class

--- immigration_status/views.py
+++ immigration_status/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import ImmigrationStatus
+from .serializers import ImmigrationStatusSerializer
 
 # Create your views here.
+# -----------------------------------------------------------------------------
+
+# ImmigrationStatusViewSet provides list, create, retrieve,
+# update, and destroy actions automatically via ModelViewSet
+# --------------------------------------------
+
+class ImmigrationStatusViewSet(viewsets.ModelViewSet):
+    # Retrieve all ImmigrationStatus records from the database
+    # --------------------------------------------
+    queryset = ImmigrationStatus.objects.all()
+    # Use ImmigrationStatusSerializer to convert records to/from JSON
+    # --------------------------------------------
+    serializer_class = ImmigrationStatusSerializer
+
