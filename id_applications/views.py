cba84432fd5bb3e2cca55d256dfc85e9568bdd60
Veries Seals <veries@me.com>
2026-04-11 11:58:20 -0500

    In id_application in views.py created class IDApllicationViewSet, defined queryset and serializer class.

--- id_applications/views.py
+++ id_applications/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import IDApplication, IDApplicationSerializer
 
 # Create your views here.
+# ---------------------------------------------------------------
+
+# Using ModelViewSet to automatically provide list, create, retrieve, update, and destroy actions.
+# This allows us to easily manage our IDApplication objects through the API without having to write additional code for each action.
+# ---------------------------------------------------------------
+class IDApplicationViewSet(viewsets.ModelViewSet):
+    # The queryset defines the set of data that will be used for the viewset. In this case, we are retrieving all IDApplication objects from the database.
+    # ---------------------------------------------------------------
+    queryset = IDApplication.objects.all()
+    # The serializer_class specifies the serializer that will be used to convert the IDApplication objects to and from JSON format. This allows us to easily serialize and deserialize our data when interacting with the API.
+    # ---------------------------------------------------------------
+    serializer_class = IDApplicationSerializer
