1105164227642a73f1e4a927c041319bd599d3a8
Veries Seals <veries@me.com>
2026-04-11 19:15:37 -0500

    In naturalization views.py created class NaturalizationRecordViewSet and defined queryset and serializer_class

--- naturalization/views.py
+++ naturalization/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import NaturalizationRecord
+from .serializers import NaturalizationRecordSerializer
+
 
 # Create your views here.
+# ---------------------------------------------
+
+# NaturalizationRecordViewSet provides list, create, retrieve,
+# update, and destroy actions automatically via ModelViewSet
+# --------------------------------------------
+
+class NaturalizationRecordViewSet(viewsets.ModelViewSet):
+    # Define the queryset to retrieve all NaturalizationRecord instances
+    # --------------------------------------------
+    queryset = NaturalizationRecord.objects.all()
+    # Specify the serializer class to be used for converting model
+    # instances to JSON and vice versa
+    # --------------------------------------------
+    serializer_class = NaturalizationRecordSerializer
